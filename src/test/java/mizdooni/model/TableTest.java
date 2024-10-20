b8ff566d577a12147798d9345eb642bb409b8c52
amirmfarzane <amirmfarzane@gmail.com>
2024-10-20 11:03:08 +0330

    Add test file and related testing libraries

---
+++ src/test/java/mizdooni/model/TableTest.java
@@ -0,0 +1,11 @@
+package mizdooni.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class TableTest {
+}
