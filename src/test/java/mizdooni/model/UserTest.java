083dda5a2c6075c242b6ebb8c0cd70744b927b9a
amirmfarzane <amirmfarzane@gmail.com>
2024-10-20 12:30:10 +0330

    Add setup for user test

--- src/test/java/mizdooni/model/UserTest.java
+++ src/test/java/mizdooni/model/UserTest.java
@@ -1,11 +1,37 @@
 package mizdooni.model;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
-public class UserTest {
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+class UserTest {
+
+    private User user;
+    private Address address;
+    private Restaurant restaurant;
+
+    @BeforeEach
+    void setUp() {
+        address = new Address("Engela Square ", "Tehran", "12345");
+
+        User manager = new User("managerUser", "managerPass", "manager@example.com", address, User.Role.manager);
+
+        restaurant = new Restaurant(
+                "Test Restaurant",
+                manager,
+                "Fastfood",
+                LocalTime.of(9, 0),
+                LocalTime.of(23, 0),
+                "Shila fastfood",
+                address,
+                "imageLink.jpg"
+        );
+
+        user = new User("testUser", "userPass", "user@example.com", address, User.Role.client);
+    }
 }
