89923a7b1d50a61dfb8069be4891c1a6c21dc7b3
amirmfarzane <amirmfarzane@gmail.com>
2024-11-15 20:34:01 +0330

    Add test for EqualsSameObject

--- Fesadyab/src/test/java/domain/TransactionTest.java
+++ Fesadyab/src/test/java/domain/TransactionTest.java
@@ -9,4 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TransactionTest {
+
+    @Test
+    public void testEqualsSameObject() {
+        Transaction txn = new Transaction();
+        txn.setTransactionId(1);
+        assertTrue(txn.equals(txn));
+    }
+
 }
