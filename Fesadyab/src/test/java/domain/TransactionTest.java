b8e998f3b8d4bef700d3401e6cfc79be75f4a2fc
amirmfarzane <amirmfarzane@gmail.com>
2024-11-15 20:32:48 +0330

    Create TransactionTest.java

---
+++ Fesadyab/src/test/java/domain/TransactionTest.java
@@ -0,0 +1,12 @@
+package domain;
+
+//import org.junit.jupiter;
+
+import domain.Transaction;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TransactionTest {
+}
